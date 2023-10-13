62991d1d777e124d23e9cda4b4c26360a996d3cc
tiendm-2604 <dao.minh.tien@sun-asterisk.com>
2023-10-13 18:45:16 +0700

    [13/10/2023][style]: update nvim tree

--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -1,6 +1,3 @@
-local HEIGHT_RATIO = 0.8 -- You can change this
-local WIDTH_RATIO = 0.5  -- You can change this too
-
 require('nvim-tree').setup({
     filters = {
         dotfiles = false,      -- show dot files
