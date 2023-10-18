de31e4bd8e9fdfbd477a3a9c77c2528e2f356f54
tiendm-2604 <dao.minh.tien@sun-asterisk.com>
2023-10-18 14:14:40 +0700

    [18/10/23][fix]: fix lsp zero

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -1,18 +1,15 @@
 local lsp_zero = require('lsp-zero')
-lsp_zero.extend_lspconfig()
 
 lsp_zero.on_attach(function(client, bufnr)
+    -- see :help lsp-zero-keybindings
+    -- to learn the available actions
     lsp_zero.default_keymaps({ buffer = bufnr })
-
-    -- lsp auto format when save five
-    -- lsp_zero.buffer_autoformat()
 end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tailwindcss', 'volar', 'grammarly', 'stylelint_lsp', 'lua_ls', 'cssls', 'eslint',
-        'html', 'jsonls', 'biome' },
-
+    ensure_installed = { 'tailwindcss', 'volar', 'grammarly', 'stylelint_lsp', 'lua_ls', 'cssls', 'eslint', 'html',
+        'jsonls', 'biome', 'tsserver' },
     automatic_installation = true,
 
     handlers = {
