b8e82b9b005a1bffbab86b972e5a502d9413fa56
tiendm-2604 <dao.minh.tien@sun-asterisk.com>
2023-10-24 13:30:59 +0700

    [24/10/23][feat]: update vim notify

--- plugin/notify.lua
+++ plugin/notify.lua
@@ -1,7 +1,22 @@
 local status, notify = pcall(require, "notify")
 
 if not status then
-	return
+    return
 end
+
+notify.setup({
+    timeout = 1800,
+
+    fps = 60,
+
+    on_open = function(win)
+        local buf = vim.api.nvim_win_get_buf(win)
+        vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+    end,
+
+    stages = "fade",
+
+    top_down = false,
+})
 
 vim.notify = notify
