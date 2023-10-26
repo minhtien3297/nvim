20f61ba390ef3d1545603032c139a5c1677e11fb
tiendm-2604 <dao.minh.tien@sun-asterisk.com>
2023-10-26 18:07:32 +0700

    [26/10/23][feat]: fix nvim surround

---
+++ README.md
@@ -0,0 +1,7 @@
+# Install
+
+<code> git clone git@github.com:minhtien3297/nvim.git </code>
+
+# Uninstall
+
+<code> rm -rf ~/.config/nvim && rm -rf ~/.local/share/nvim </code>
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,219 +1,222 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    -- theme
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            vim.cmd([[colorscheme catppuccin-mocha]])
-        end,
-    },
+	-- theme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			vim.cmd([[colorscheme catppuccin-mocha]])
+		end,
+	},
 
-    -- ui input and select
-    {
-        "stevearc/dressing.nvim",
-        event = "VeryLazy",
-        opts = {},
-    },
+	-- ui input and select
+	{
+		"stevearc/dressing.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
 
-    -- tag highlight
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
+	-- tag highlight
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
 
-    -- sticky scroll
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-    },
+	-- sticky scroll
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+	},
 
-    -- status bar
-    {
-        "nvim-lualine/lualine.nvim",
-    },
+	-- status bar
+	{
+		"nvim-lualine/lualine.nvim",
+	},
 
-    -- find files
-    {
-        "nvim-telescope/telescope.nvim",
-        tag = "0.1.4",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
+	-- find files
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.4",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    -- noti
-    {
-        "rcarriga/nvim-notify",
-    },
+	-- noti
+	{
+		"rcarriga/nvim-notify",
+	},
 
-    -- file navigation
-    {
-        "ThePrimeagen/harpoon",
-    },
+	-- file navigation
+	{
+		"ThePrimeagen/harpoon",
+	},
 
-    -- file icons
-    {
-        "nvim-tree/nvim-web-devicons",
-    },
+	-- file icons
+	{
+		"nvim-tree/nvim-web-devicons",
+	},
 
-    -- highlight color
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("colorizer").setup()
-        end,
-    },
+	-- highlight color
+	{
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup()
+		end,
+	},
 
-    -- highlight indent
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        opts = {},
-    },
+	-- highlight indent
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		main = "ibl",
+		opts = {},
+	},
 
-    -- fold manager
-    {
-        "kevinhwang91/nvim-ufo",
-        dependencies = {
-            "kevinhwang91/promise-async",
-            "luukvbaal/statuscol.nvim",
-        },
-    },
+	-- fold manager
+	{
+		"kevinhwang91/nvim-ufo",
+		dependencies = {
+			"kevinhwang91/promise-async",
+			"luukvbaal/statuscol.nvim",
+		},
+	},
 
-    -- auto pairs
-    {
-        "windwp/nvim-autopairs",
-        event = "InsertEnter",
-        opts = {},
-    },
+	-- auto pairs
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		opts = {},
+	},
 
-    -- smooth scroll
-    {
-        "karb94/neoscroll.nvim",
-    },
+	-- smooth scroll
+	{
+		"karb94/neoscroll.nvim",
+	},
 
-    -- Comment
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-    },
+	-- Comment
+	{
+		"numToStr/Comment.nvim",
+		opts = {},
+	},
 
-    -- markdown preview
-    {
-        "iamcco/markdown-preview.nvim",
-        cmd = {
-            "MarkdownPreviewToggle",
-            "MarkdownPreview",
-            "MarkdownPreviewStop",
-        },
-        ft = { "markdown" },
-        build = function()
-            vim.fn["mkdp#util#install"]()
-        end,
-    },
+	-- markdown preview
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = {
+			"MarkdownPreviewToggle",
+			"MarkdownPreview",
+			"MarkdownPreviewStop",
+		},
+		ft = { "markdown" },
+		build = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	},
 
-    -- highlight todo
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
+	-- highlight todo
+	{
+		"folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {},
+	},
 
-    -- git ignore
-    {
-        "wintermute-cell/gitignore.nvim",
-    },
+	-- git ignore
+	{
+		"wintermute-cell/gitignore.nvim",
+	},
 
-    -- git fugitive
-    {
-        "tpope/vim-fugitive",
-    },
+	-- git fugitive
+	{
+		"tpope/vim-fugitive",
+	},
 
-    -- gitsigns
-    {
-        "lewis6991/gitsigns.nvim",
-    },
+	-- gitsigns
+	{
+		"lewis6991/gitsigns.nvim",
+	},
 
-    -- tmux navigator
-    {
-        "christoomey/vim-tmux-navigator",
-    },
+	-- tmux navigator
+	{
+		"christoomey/vim-tmux-navigator",
+	},
 
-    -- editor configs
-    {
-        "editorconfig/editorconfig-vim",
-    },
+	-- editor configs
+	{
+		"editorconfig/editorconfig-vim",
+	},
 
-    -- command lsp
-    {
-        "gelguy/wilder.nvim",
-    },
+	-- command lsp
+	{
+		"gelguy/wilder.nvim",
+	},
 
-    -- folder tree
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-    },
+	-- folder tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 
-    -- vim surround
-    {
-        "kylechui/nvim-surround",
-        version = "*",
-        event = "VeryLazy",
-    },
+	-- vim surround
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 
-    -- auto save
-    {
-        "okuuva/auto-save.nvim",
-        cmd = "ASToggle",
-        event = { "InsertLeave", "TextChanged", "BufLeave", "FocusLost" },
-        opts = {},
-    },
+	-- auto save
+	{
+		"okuuva/auto-save.nvim",
+		cmd = "ASToggle",
+		event = { "InsertLeave", "TextChanged", "BufLeave", "FocusLost" },
+		opts = {},
+	},
 
-    --formatter
-    {
-        "mhartington/formatter.nvim",
-        event = "VeryLazy",
-    },
+	--formatter
+	{
+		"mhartington/formatter.nvim",
+		event = "VeryLazy",
+	},
 
-    -- lsp
-    {
-        { "williamboman/mason.nvim" },
-        { "williamboman/mason-lspconfig.nvim" },
-        { "neovim/nvim-lspconfig" },
-        { "VonHeikemen/lsp-zero.nvim",        branch = "v3.x" },
-        { "hrsh7th/cmp-nvim-lsp" },
-        { "hrsh7th/nvim-cmp" },
-        {
-            "L3MON4D3/LuaSnip",
-            dependencies = { "rafamadriz/friendly-snippets" },
-        },
-        { "onsails/lspkind.nvim" },
-    },
+	-- lsp
+	{
+		{ "williamboman/mason.nvim" },
+		{ "williamboman/mason-lspconfig.nvim" },
+		{ "neovim/nvim-lspconfig" },
+		{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/nvim-cmp" },
+		{
+			"L3MON4D3/LuaSnip",
+			dependencies = { "rafamadriz/friendly-snippets" },
+		},
+		{ "onsails/lspkind.nvim" },
+	},
 
-    -- which key
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-    },
+	-- which key
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		init = function()
+			vim.o.timeout = true
+			vim.o.timeoutlen = 300
+		end,
+	},
 })
