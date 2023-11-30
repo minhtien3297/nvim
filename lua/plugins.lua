a45ebe15609c5710b4ea6f1d7f90c06b79ae8617
tiendm-2604 <dao.minh.tien@sun-asterisk.com>
2023-11-30 11:17:50 +0700

    [30/11/2023][feat]: update nvim tree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -157,6 +157,18 @@ require("lazy").setup({
         },
     },
 
+    -- lsp nvim tree
+    {
+        "antosha417/nvim-lsp-file-operations",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-tree.lua",
+        },
+        config = function()
+            require("lsp-file-operations").setup()
+        end,
+    },
+
     -- vim surround
     {
         "kylechui/nvim-surround",
--- plugin/nvim-tree.lua
+++ plugin/nvim-tree.lua
@@ -64,6 +64,71 @@ local function my_on_attach(bufnr)
         api.tree.focus()
     end
 
+    -- mark operation
+    local mark_move_j = function()
+        api.marks.toggle()
+        vim.cmd("norm j")
+    end
+    local mark_move_k = function()
+        api.marks.toggle()
+        vim.cmd("norm k")
+    end
+
+    -- marked files operation
+    local mark_trash = function()
+        local marks = api.marks.list()
+        if #marks == 0 then
+            table.insert(marks, api.tree.get_node_under_cursor())
+        end
+        vim.ui.input({ prompt = string.format("Trash %s files? [y/n] ", #marks) }, function(input)
+            if input == "y" then
+                for _, node in ipairs(marks) do
+                    api.fs.trash(node)
+                end
+                api.marks.clear()
+                api.tree.reload()
+            end
+        end)
+    end
+    local mark_remove = function()
+        local marks = api.marks.list()
+        if #marks == 0 then
+            table.insert(marks, api.tree.get_node_under_cursor())
+        end
+        vim.ui.input({ prompt = string.format("Remove/Delete %s files? [y/n] ", #marks) }, function(input)
+            if input == "y" then
+                for _, node in ipairs(marks) do
+                    api.fs.remove(node)
+                end
+                api.marks.clear()
+                api.tree.reload()
+            end
+        end)
+    end
+
+    local mark_copy = function()
+        local marks = api.marks.list()
+        if #marks == 0 then
+            table.insert(marks, api.tree.get_node_under_cursor())
+        end
+        for _, node in pairs(marks) do
+            api.fs.copy.node(node)
+        end
+        api.marks.clear()
+        api.tree.reload()
+    end
+    local mark_cut = function()
+        local marks = api.marks.list()
+        if #marks == 0 then
+            table.insert(marks, api.tree.get_node_under_cursor())
+        end
+        for _, node in pairs(marks) do
+            api.fs.cut(node)
+        end
+        api.marks.clear()
+        api.tree.reload()
+    end
+
     -- default mapping
     api.config.mappings.default_on_attach(bufnr)
 
@@ -71,6 +136,14 @@ local function my_on_attach(bufnr)
     vim.keymap.set("n", "l", edit_or_open, opts("Edit Or Open"))
     vim.keymap.set("n", "L", vsplit_preview, opts("Vsplit Preview"))
     vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Collapse"))
+    vim.keymap.set("n", "p", api.fs.paste, opts("Paste"))
+    vim.keymap.set("n", "J", mark_move_j, opts("Toggle Bookmark Down"))
+    vim.keymap.set("n", "K", mark_move_k, opts("Toggle Bookmark Up"))
+    vim.keymap.set("n", "dd", mark_cut, opts("Cut File(s)"))
+    vim.keymap.set("n", "df", mark_trash, opts("Trash File(s)"))
+    vim.keymap.set("n", "dF", mark_remove, opts("Remove File(s)"))
+    vim.keymap.set("n", "yy", mark_copy, opts("Copy File(s)"))
+    vim.keymap.set("n", "mv", api.marks.bulk.move, opts("Move Bookmarked"))
 end
 
 nvim_tree.setup({
