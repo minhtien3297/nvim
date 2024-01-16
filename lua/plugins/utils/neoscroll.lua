72e266c9b518f35cfa14c28ee42a3c7bb4d36acf
minhtien <minhitne@gmai.com>
2024-01-16 15:16:34 +0700

    [fix]: neoscroll config

--- lua/plugins/utils/neoscroll.lua
+++ lua/plugins/utils/neoscroll.lua
@@ -2,7 +2,6 @@ return {
   "karb94/neoscroll.nvim",
   event = "VeryLazy",
   opts = {
-    mappings = { '<C-u>', '<C-d>', '<C-b>',
-      '<C-y>', '<C-e>', 'zt', 'zz', 'zb' },
+    mappings = { '<C-u>', '<C-d>' },
   }
 }
