21a78dfa7d4cbdbf9092d2d9e678ee8b0be264b8
minhtien3297 <minhitne@gmail.com>
2024-03-21 16:31:40 +0700

    [feat]: sync

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -1,36 +1,37 @@
 local slow_format_filetypes = {}
+
 return {
-  "stevearc/conform.nvim",
-  event = { "BufWritePre" },
-  cmd = { "ConformInfo" },
-  opts = {
-    formatters_by_ft = {
-      lua = { "stylua" },
-      javascript = { { "prettierd", "prettier" } },
-      html = { "htmlbeautifier" },
-      bash = { "beautysh" },
-      ["*"] = { "codespell" },
-      ["_"] = { "trim_whitespace", "trim_newlines" },
-    },
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			javascript = { { "prettierd", "prettier" } },
+			html = { "htmlbeautifier" },
+			bash = { "beautysh" },
+			["*"] = { "codespell" },
+			["_"] = { "trim_whitespace", "trim_newlines" },
+		},
 
-    format_on_save = function(bufnr)
-      if slow_format_filetypes[vim.bo[bufnr].filetype] then
-        return
-      end
-      local function on_format(err)
-        if err and err:match("timeout$") then
-          slow_format_filetypes[vim.bo[bufnr].filetype] = true
-        end
-      end
+		format_on_save = function(bufnr)
+			if slow_format_filetypes[vim.bo[bufnr].filetype] then
+				return
+			end
+			local function on_format(err)
+				if err and err:match("timeout$") then
+					slow_format_filetypes[vim.bo[bufnr].filetype] = true
+				end
+			end
 
-      return { timeout_ms = 200, lsp_fallback = true }, on_format
-    end,
+			return { timeout_ms = 200, lsp_fallback = true }, on_format
+		end,
 
-    format_after_save = function(bufnr)
-      if not slow_format_filetypes[vim.bo[bufnr].filetype] then
-        return
-      end
-      return { lsp_fallback = true }
-    end,
-  },
+		format_after_save = function(bufnr)
+			if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+				return
+			end
+			return { lsp_fallback = true }
+		end,
+	},
 }
