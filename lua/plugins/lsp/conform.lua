23362cf34bd05daa5dae29b2aeb534c84739750f
minhtien3297 <minhitne@gmail.com>
2024-03-23 16:33:22 +0700

    [fix]: remove speedtyper for error with UI

--- lua/plugins/lsp/conform.lua
+++ lua/plugins/lsp/conform.lua
@@ -18,6 +18,7 @@ return {
 			if slow_format_filetypes[vim.bo[bufnr].filetype] then
 				return
 			end
+
 			local function on_format(err)
 				if err and err:match("timeout$") then
 					slow_format_filetypes[vim.bo[bufnr].filetype] = true
